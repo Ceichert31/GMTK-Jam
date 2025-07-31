7de46e955dee317d160a59877998a216a5bc59a6
Jared Orrantia <jorr8785@gmail.com>
2025-07-31 13:34:47 -0700

    added to moveable object
    made a text object to tell the player what they need to move the object

--- GMTKJam/Assets/Scripts/ItemDetector.cs
+++ GMTKJam/Assets/Scripts/ItemDetector.cs
@@ -20,5 +20,9 @@ public class ItemDetector : MonoBehaviour
                 door.MoveObject();
             }
         }
+        else
+        {
+            door.warningTextUp();
+        }
     }
 }
