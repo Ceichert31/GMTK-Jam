e8b56bacf29d87f4071d82e44b7fb27463bd4215
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-07-30 21:22:41 -0400

    feat: Added event channel package

---
+++ GMTKJam/Assets/Scripts/LevelController.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    //Manage the players current level and move player between levels
+    //We will need a list of all levels and their spawn points as well as the number of levels
+    //We can use modulus to prevent the players current level from going out of scope
+}
