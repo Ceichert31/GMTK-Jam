7db8c4282bb46ec96fbd18e82ebda1a5b6a7fd95
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-08-03 03:04:54 -0400

    fix: Camera shake bug

--- GMTKJam/Assets/Scripts/CollectCrysal.cs
+++ GMTKJam/Assets/Scripts/CollectCrysal.cs
@@ -9,16 +9,23 @@ public class CollectCrysal : MonoBehaviour, IResetable
     [SerializeField]
     private GameObject arrow;
 
+    private bool isEnabled = true;
+
     public void Reset()
     {
         arrow.transform.DOScaleY(0, 0f);
         DOTween.CompleteAll();
+        isEnabled = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!isEnabled)
+            return;
+
         if (collision.gameObject.layer == playerLayer)
         {
+            isEnabled = false;
             arrow.transform.DOScaleY(18, 0.5f);
             CameraShakeManager.Instance.Shake(1f, 2f);
             GetComponent<AudioSource>().Play();
